d29e3e92c4f5d056f25cdc3f70bfdfb74a273c64
Nguyễn Lam <nguyenlam@192.168.2.46>
2023-12-10 11:20:05 +0700

    customers

--- src/Controller/ChiphiController.php
+++ src/Controller/ChiphiController.php
@@ -12,19 +12,68 @@ class ChiphiController extends AbstractController
     #[Route('/chiphi', name: 'chiphi')]
     public function index(Connection $connection): Response
     {
-        $sql = "SELECT PurchaseInvoices.PurchaseInvoiceID, PurchaseInvoices.DistributorID, 
-        PurchaseInvoices.Date, PurchaseInvoices.ExpenseType, 
-        FORMAT(PurchaseInvoices.Amount, 2) AS Amount, Distributors.DistributorName
- FROM PurchaseInvoices
- JOIN Distributors ON PurchaseInvoices.DistributorID = Distributors.DistributorID;        
- ";
+        // Truy vấn để lấy ra tổng TotalEarnings với PaymentStatus = 1 cho tháng hiện tại
+        $sqlTotalEarningsPaid = "SELECT SUM(TotalEarnings) AS TotalEarningsPaid FROM Salary WHERE PaymentStatus = 1 AND DATE_FORMAT(Date, '%Y-%m') = DATE_FORMAT(NOW(), '%Y-%m')";
+        $totalEarningsPaid = $connection->executeQuery($sqlTotalEarningsPaid)->fetchOne();
 
-$result = $connection->executeQuery($sql)->fetchAllAssociative();
+        // Truy vấn để lấy ra tổng TotalEarnings với PaymentStatus = 0 cho tháng hiện tại
+        $sqlTotalEarningsNotPaid = "SELECT SUM(TotalEarnings) AS TotalEarningsNotPaid FROM Salary WHERE PaymentStatus = 0 AND DATE_FORMAT(Date, '%Y-%m') = DATE_FORMAT(NOW(), '%Y-%m')";
+        $totalEarningsNotPaid = $connection->executeQuery($sqlTotalEarningsNotPaid)->fetchOne();
 
+        // Truy vấn để lấy ra tổng TotalEarnings với PaymentStatus = 1 cho tháng trước
+        $sqlTotalEarningsPaidLastMonth = "SELECT SUM(TotalEarnings) AS TotalEarningsPaidLastMonth FROM Salary WHERE PaymentStatus = 1 AND DATE_FORMAT(Date, '%Y-%m') = DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 1 MONTH), '%Y-%m')";
+        $totalEarningsPaidLastMonth = $connection->executeQuery($sqlTotalEarningsPaidLastMonth)->fetchOne();
+
+        // Truy vấn để lấy ra tổng TotalEarnings với PaymentStatus = 0 cho tháng trước
+        $sqlTotalEarningsNotPaidLastMonth = "SELECT SUM(TotalEarnings) AS TotalEarningsNotPaidLastMonth FROM Salary WHERE PaymentStatus = 0 AND DATE_FORMAT(Date, '%Y-%m') = DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 1 MONTH), '%Y-%m')";
+        $totalEarningsNotPaidLastMonth = $connection->executeQuery($sqlTotalEarningsNotPaidLastMonth)->fetchOne();
+
+
+           // Truy vấn để lấy ra tổng Amount của PurchaseInvoices cho tháng hiện tại
+           $sqlTotalExpenseThisMonth = "SELECT SUM(Amount) AS TotalExpenseThisMonth FROM PurchaseInvoices WHERE DATE_FORMAT(Date, '%Y-%m') = DATE_FORMAT(NOW(), '%Y-%m')";
+           $totalExpenseThisMonth = $connection->executeQuery($sqlTotalExpenseThisMonth)->fetchOne();
+   
+           // Truy vấn để lấy ra tổng Amount của PurchaseInvoices cho tháng trước
+           $sqlTotalExpenseLastMonth = "SELECT SUM(Amount) AS TotalExpenseLastMonth FROM PurchaseInvoices WHERE DATE_FORMAT(Date, '%Y-%m') = DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 1 MONTH), '%Y-%m')";
+           $totalExpenseLastMonth = $connection->executeQuery($sqlTotalExpenseLastMonth)->fetchOne();
+   
+           // Tính toán % so với tháng trước
+           $percentageExpenseChange = 0;
+           if ($totalExpenseLastMonth != 0) {
+               $percentageExpenseChange = (($totalExpenseThisMonth - $totalExpenseLastMonth) / $totalExpenseLastMonth) * 100;
+           }
+   
+           // Round the percentage to 2 decimal places
+           $percentageExpenseChange = round($percentageExpenseChange, 2);
+   
+        // Tính toán % so với tháng trước
+        $percentageChange = 0;
+        if ($totalEarningsPaidLastMonth != 0) {
+            $percentageChange = (($totalEarningsPaid - $totalEarningsPaidLastMonth) / $totalEarningsPaidLastMonth) * 100;
+        }
+
+        // Round the percentage to 2 decimal places
+        $percentageChange = round($percentageChange, 2);
+
+        // Truy vấn thông tin chi phí từ bảng PurchaseInvoices và Distributors
+        $sqlChiphi = "SELECT PurchaseInvoices.PurchaseInvoiceID, PurchaseInvoices.DistributorID, 
+            PurchaseInvoices.Date, PurchaseInvoices.ExpenseType, 
+            FORMAT(PurchaseInvoices.Amount, 2) AS Amount, Distributors.DistributorName
+            FROM PurchaseInvoices
+            JOIN Distributors ON PurchaseInvoices.DistributorID = Distributors.DistributorID";
+        $result = $connection->executeQuery($sqlChiphi)->fetchAllAssociative();
 
         return $this->render('chiphi/index.html.twig', [
             'controller_name' => 'ChiphiController',
             'result' => $result,
+            'totalEarningsPaid' => $totalEarningsPaid,
+            'totalEarningsNotPaid' => $totalEarningsNotPaid,
+            'totalEarningsPaidLastMonth' => $totalEarningsPaidLastMonth,
+            'totalEarningsNotPaidLastMonth' => $totalEarningsNotPaidLastMonth,
+            'percentageChange' => $percentageChange,
+            'totalExpenseThisMonth' => $totalExpenseThisMonth,
+            'totalExpenseLastMonth' => $totalExpenseLastMonth,
+            'percentageExpenseChange' => $percentageExpenseChange,
         ]);
     }
 }
--- src/Controller/LuongController.php
+++ src/Controller/LuongController.php
@@ -2,17 +2,166 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class LuongController extends AbstractController
 {
     #[Route('/luong', name: 'luong')]
-    public function index(): Response
+    public function index(Connection $connection, Request $request): Response
     {
+        // Đoạn SQL cần thực hiện
+        $sqlQuery = "
+        SELECT
+        Users.UserID,
+        Users.FullName,
+        Users.CreatedDate,
+        FORMAT(RolePermissions.Salary, 0) AS 'Salary',
+        COUNT(DISTINCT Attendance.Date) AS 'TotalDays',
+        IFNULL(Salary.PaymentStatus, 0) AS 'PaymentStatus',
+        Salary.SalaryID  -- Add SalaryID to the SELECT statement
+    FROM Users
+    JOIN UserRoles ON Users.UserID = UserRoles.UserID
+    JOIN RolePermissions ON UserRoles.RoleID = RolePermissions.RoleID
+    LEFT JOIN Attendance ON Users.UserID = Attendance.UserID
+    LEFT JOIN Salary ON Users.UserID = Salary.UserID
+    GROUP BY Users.UserID, Users.FullName, Users.CreatedDate, RolePermissions.Salary, Salary.PaymentStatus, Salary.SalaryID;
+    
+        ";
+        $sqlPermissions = "SELECT * FROM  Permissions";
+        // Thực hiện truy vấn SQL
+        $result = $connection->executeQuery($sqlQuery)->fetchAllAssociative();
+        $permissionname = $connection->executeQuery($sqlPermissions)->fetchAllAssociative();
+        
+
+
+ // Lấy giá trị tháng từ tham số truy vấn
+ $selectedMonth = $request->query->get('selectedMonth');
+
+ // Nếu không có giá trị tháng được chọn, mặc định là 0 để lọc toàn bộ dữ liệu
+ $selectedMonth = $selectedMonth ?: 0;
+
+ // Thực hiện truy vấn với điều kiện lọc theo tháng đã chọn
+ $sql = '
+     SELECT
+         Users.UserID,
+         Users.FullName,
+         Users.CreatedDate,
+         FORMAT(RolePermissions.Salary, 0) AS Salary,
+         COUNT(DISTINCT Attendance.Date) AS TotalDays,
+         IFNULL(Salary.PaymentStatus, 0) AS PaymentStatus,
+         Salary.SalaryID,
+         Salary.Date AS PaymentDate
+     FROM Users
+     JOIN UserRoles ON Users.UserID = UserRoles.UserID
+     JOIN RolePermissions ON UserRoles.RoleID = RolePermissions.RoleID
+     LEFT JOIN Attendance ON Users.UserID = Attendance.UserID
+     LEFT JOIN Salary ON Users.UserID = Salary.UserID
+     WHERE (:selectedMonth = 0 OR MONTH(Salary.Date) = :selectedMonth)
+     GROUP BY Users.UserID, Users.FullName, Users.CreatedDate, RolePermissions.Salary, Salary.PaymentStatus, Salary.SalaryID, Salary.Date
+ ';
+
+ $params = ['selectedMonth' => $selectedMonth];
+ $types = ['selectedMonth' => \PDO::PARAM_INT];
+ $results = $connection->executeQuery($sql, $params, $types)->fetchAllAssociative();
+
+
+
+
+
+
+
+
+   
+        // Trả kết quả cho giao diện người dùng
         return $this->render('luong/index.html.twig', [
-            'controller_name' => 'LuongController',
+            'result' => $result,
+            'permissionname' => $permissionname,
+            'results' => $results,
+
+
         ]);
     }
+
+    #[Route('/update-payment-status/{SalaryID}', name: 'update_payment_status', methods: ['POST'])]
+    public function updatePaymentStatus(Connection $connection, Request $request): Response
+    {
+        // Lấy giá trị SalaryID từ yêu cầu POST
+        $data = json_decode($request->getContent(), true);
+        $SalaryID = $data['SalaryID'] ?? null;
+    
+        // Kiểm tra xem SalaryID có tồn tại không
+        if ($SalaryID !== null) {
+            // Thực hiện cập nhật trạng thái thanh toán trong bảng Salary
+            $sql = "UPDATE Salary SET PaymentStatus = 1, Date = CURRENT_DATE WHERE SalaryID = :SalaryID";
+            $params = ['SalaryID' => $SalaryID];
+    
+            // Thực hiện câu lệnh SQL và kiểm tra kết quả
+            $result = $connection->executeStatement($sql, $params);
+    
+            if ($result !== false) {
+                // Trả về kết quả dưới dạng JSON
+                return $this->json(['success' => true]);
+            }
+        }
+    
+        // Trả về kết quả khi có lỗi hoặc SalaryID không tồn tại
+        return $this->json(['success' => false, 'error' => 'Invalid SalaryID or failed to update.']);
+    }
+
+
+   
+
+
+    #[Route('/update-luong', name: 'update_luong', methods: ['POST'])]
+    #[Security('is_granted("AdminAccess")')]
+    public function updateLuong(Connection $connection, Request $request): Response
+    {
+      
+        $permissionID = $request->request->get('PermissionName');
+    $newSalary = $request->request->get('Salary');
+
+    // Thực hiện cập nhật lương dựa trên PermissionID
+    $sql = "UPDATE RolePermissions SET Salary = :newSalary WHERE PermissionID = :permissionID";
+    $params = ['newSalary' => $newSalary, 'permissionID' => $permissionID];
+
+    $result = $connection->executeStatement($sql, $params);
+
+    if ($result !== false) {
+        // Nếu cập nhật thành công, sử dụng addFlash để hiển thị thông báo
+        $this->addFlash('success', 'Lương đã được cập nhật thành công.');
+    } else {
+        // Nếu có lỗi, sử dụng addFlash để hiển thị thông báo lỗi
+        $this->addFlash('error', 'Có lỗi khi cập nhật lương.');
+    }
+
+    // Chuyển hướng hoặc hiển thị trang tương ứng
+    return $this->redirectToRoute('luong');
+    }
+
+
+
+    #[Route('/get-salary', name: 'get_salary', methods: ['POST'])]
+    public function getSalary(Connection $connection, Request $request): JsonResponse
+    {
+        $permissionID = $request->request->get('PermissionID');
+    
+        // Thực hiện câu truy vấn để lấy lương dựa trên PermissionID
+        $sql = "SELECT Salary FROM RolePermissions WHERE PermissionID = :permissionID";
+        $params = ['permissionID' => $permissionID];
+        $result = $connection->executeQuery($sql, $params)->fetchAssociative();
+    
+        $salary = $result ? number_format($result['Salary'], 0, ',', '.') : null;
+    
+        // Trả về dữ liệu lương dưới dạng JSON
+        return $this->json(['salary' => $salary]);
+    }
+    
+    
+    
 }
